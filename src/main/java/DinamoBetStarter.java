3cf826023ca6020923350877827cb78b3368abdb
MELIHDEMIRCAN <melih.demircan@consultant.turkcell.com.tr>
2023-09-24 17:47:34 +0300

    test
    test

--- src/main/java/DinamoBetStarter.java
+++ src/main/java/DinamoBetStarter.java
@@ -3,7 +3,6 @@ public class DinamoBetStarter {
     public static void main(String[] args) {
         System.out.println("Test");
 
-
         String domain = "https://www.dinamobet745.com";
 
         DinamoBetToken chromeToken = new DinamoBetToken(domain, domain + "/tr/game/everymatrix/live/play/47844030144#/");
